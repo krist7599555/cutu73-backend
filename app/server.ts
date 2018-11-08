592b58a21d9a81c33fa9f3fe44b6832a27b56c0d
Rodchananat <rod8711@gmail.com>
2018-11-09 00:27:25 +0700

    add dep : cookie-parser

--- app/server.ts
+++ app/server.ts
@@ -3,14 +3,14 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import {APIController} from './controllers/index';
-
+import cookieParser from 'cookie-parser';
 
 const port: String = process.env.PORT || '3000';
 const app = express(); // somehow i can't define type of this variable
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-
+app.use(cookieParser());
 
 app.use('/api/v1', APIController);
 app.listen(port, () => {
